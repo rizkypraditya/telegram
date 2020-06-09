c335fa6cf3badc44ac14156daf9508a8354272cc
rizkypraditya <66479226+rizkypraditya@users.noreply.github.com>
2020-06-09 23:36:47 +0800

    Create cpu.php

---
+++ webhook/php/cpu.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Contoh code CPU Bot.
+ *
+ * @author Rizky Praditya <rizky.praditya@telkom.co.id>
+ */
+
+//Memasukan Code default Sensor
+include '../default/Sensor.php';
+
+// Atur TOKEN bot disini
+$bot_token = '1074742441:AAHJJb-JYBlg-10JcmMllDrMA6LO4WtOQ3k';
+
+// Aktifkan Sensor Bot
+$sensor = new Sensor($bot_token);
+
+// Baca hasil pendengaran sensor dari setiap chat masuk
+$text = $sensor->Text(); // isi pesan
+$chat_id = $sensor->ChatID(); // id pengirim
+$first_name = $sensor->FirstName(); // nama depan pengirim
+$last_name = $sensor->LastName(); // nama belakang pengirim
+$username = $sensor->Username(); // username pengirim
+$message_id = $sensor->MessageID(); // chat id pesan
+
+// Cek apakah ada chat masuk yang sesuai daftar sensor chat kita
+if (!is_null($text) && !is_null($chat_id)) {
+    // Sensor #0 ketika ada chat berupa /start
+    if ($text == '/start') {
+        $reply = 'Selamat Datang di Bot AditDev';
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor #1 ketika ada chat berupa /semangat
+    elseif ($text == '/semangat') {
+        $reply = 'Semangat Pagi Indonesia !';
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor #2 ketika ada chat berupa /siapa
+    elseif ($text == '/siapa') {
+        $reply = 'Nama Kamu adalah '.$first_name;
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor #3 ketika ada chat berupa /berapa
+    elseif ($text == '/berapa') {
+        $reply = 'Nomor telegram Kamu adalah '.$chat_id;
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor #4 ketika ada chat berupa /lengkap
+    elseif ($text == '/lengkap') {
+        $reply = 'Nama lengkap Kamu adalah '.$first_name.' '.$last_name;
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor #BONUS ketika ada chat berupa /enter
+    elseif ($text == '/enter') {
+        $reply = 'Hai, '.$first_name."\n\n\napa kabar?";
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+    // Sensor terakhir ketika ada chat yang belum terdaftar
+    else {
+        $reply = 'Mohon maaf pesan anda tidak kami kenali';
+        // Kirim balasan konten
+        $content = ['chat_id' => $chat_id, 'text' => $reply];
+        $sensor->sendMessage($content);
+    } 
+}
