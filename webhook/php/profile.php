458793c832cfa52bdccadecccbbcdbbbaaca7faf
rizkypraditya <66479226+rizkypraditya@users.noreply.github.com>
2022-01-29 14:48:16 +0800

    Create profile.php

---
+++ webhook/php/profile.php
@@ -0,0 +1,20 @@
+<!-- Library wajib -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../default/style.css">
+
+<div class="card">
+    <!-- Masukan nama file foto-->
+  <img src="foto.png" alt="Foto" style="width:100%">
+  <!-- Masukan Nama kamu-->
+  <h1>Nama Kamu</h1>
+  <!-- Masukan Jabatan / Tingkat Pendidikan kamu-->
+  <p class="title">Jabatan Kamu</p>
+  <!-- Masukan Instansi / Sekolah kamu-->
+  <p>Instansi Kamu</p>
+  <!-- Masukan URL Profil Sosmed kamu-->
+  <a href="#"><i class="fa fa-instagram"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <p><button>Contact</button></p>
+</div>
